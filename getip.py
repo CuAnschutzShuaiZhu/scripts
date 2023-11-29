bc8d81e35b49d9186eca3a127c0892ac6238a1f3
CuAnschutzShuaiZhu <144367273+CuAnschutzShuaiZhu@users.noreply.github.com>
2023-11-29 20:27:06 +0000

    update

--- getip.py
+++ getip.py
@@ -1,3 +1,3 @@
 from requests import get
-ip = get('https://api.ipify.org').text
-print('My public IP address is: {}'.format(ip))
+
+print('My public IP address is: {}'.format(get('https://api.ipify.org').text))
